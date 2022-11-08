7d9ef3652b043c16fe765766a76b81e517e4be1b
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-11-08 17:15:45 -0600

    Reassign variables and practice console logs

--- variables.js
+++ variables.js
@@ -9,3 +9,12 @@ var num3 = 3;
 var dinnerIsReady = false;
 var isHungry = true;
 var ovenPreheatOn = true;
+
+stringyPasta = "fettuccine";
+tubeyPasta = "macaroni";
+potatoPasta = "uh, oh still gnocchi";
+
+console.log("For dinner tonight we have " + stringyPasta);
+console.log("Battery is at " + num1 + "%");
+console.log(ovenPreheatOn);
+console.log("Again for dinner tonight we have " + potatoPasta);
